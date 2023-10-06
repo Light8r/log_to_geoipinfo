3623c68e6f96074c81908ba943827898949d8b91
Wright <63088747+Light8r@users.noreply.github.com>
2023-10-06 21:01:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import re, requests, json, sys，time
+import requests, json, time, datetime, sys
 
 api_url = "https://ipgeolocation.abstractapi.com/v1/"
 api_key = sys.argv[1]
-content_group = []
-ip_list = []
+connection_analysis = {}
+HOST_NUMBER = 10
 
 
 def get_geolocation_info(validated_ip_address):
@@ -19,31 +19,54 @@ def get_geolocation_info(validated_ip_address):
 fileopen = open("naive.log")
 line = fileopen.readline()
 while line:
-    flag = line.find("remote_ip")
-    if flag != -1:
-        ipfounded = re.search(
-            r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", line[flag:-1])
-        if ipfounded.group() not in ip_list:
-            ip_list.append(ipfounded.group())
+    data = json.loads(line)
+    request = data['request']
+    remote_ip = request['remote_ip']
+    timestamp = data['ts']
+    host = request['host']
+    if remote_ip not in connection_analysis:
+        ipdata = json.loads(get_geolocation_info(remote_ip))
+        time.sleep(1)
+        city = ipdata['city']
+        country = ipdata['country']
+        connection = ipdata['connection']
+        isp_name = connection['isp_name']
+        connection_type = connection['connection_type']
+        host_analysis = {host: 1}
+        ipinfo = {'time': time.strftime("%Y-%m-%d %H:%M:%S", (time.localtime(timestamp + datetime.timedelta(hours=8)))),
+                  'host_analysis': host_analysis,
+                  'city': city,
+                  'country': country,
+                  'isp_name': isp_name,
+                  'connection_type': connection_type
+                  }
+        connection_analysis[remote_ip] = ipinfo
+    else:
+        ipinfo = connection_analysis[remote_ip]
+        ipinfo['time'] = time.strftime("%Y-%m-%d %H:%M:%S", (time.localtime(timestamp + datetime.timedelta(hours=8))))
+        host_analysis = ipinfo['host_analysis']
+        if host not in host_analysis:
+            host_analysis.setdefault(host, 1)
+        else:
+            host_analysis[host] += 1
+        ipinfo['host_analysis'] = host_analysis
+        connection_analysis[remote_ip] = ipinfo
     line = fileopen.readline()
-fileopen.close()
-
-ip_list.sort()
-for item in ip_list:
-    data = json.loads(get_geolocation_info(item))
-    time.sleep(1)
-    try:
-        ip = data['ip_address']
-        city = data['city']
-        content_group.append(ip + ' : ' + city)
-    except KeyError:
-        content_group.append(item+" : No data")
-for item in content_group:
-    outfile = open("out.txt", "w")
-    print(item)
-    outfile.write(item)
-    outfile.close()
-if len(content_group)!=0:
+for ip, analysis in connection_analysis.items():
+    host_analysis = analysis['host_analysis']
+    plist = sorted(host_analysis.items(), key=lambda x: (x[1], x[0]), reverse=True)
+    plist = plist[:min(HOST_NUMBER, len(host_analysis))]
+    tmp = {}
+    for item in plist:
+        tmp[item[0]] = item[1]
+    host_analysis = tmp
+    analysis['host_analysis'] = host_analysis
+    connection_analysis[ip] = analysis
+Output = json.dumps(connection_analysis, sort_keys=True, indent=4, separators=(',', ':'))
+fileout = open("out.json", "w", encoding="utf-8")
+fileout.write(Output)
+fileout.close()
+if len(connection_analysis)!=0:
     print("Your ip has been successfully converted to GeoIpLocation into out.txt")
 else:
     print("No ip found in log file!")
