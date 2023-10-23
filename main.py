d9ada77f78e6c424d753bb32ad6d2237711efe5f
Wright <63088747+Light8r@users.noreply.github.com>
2023-10-23 10:14:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,39 +19,42 @@ def get_geolocation_info(validated_ip_address):
 fileopen = open("naive.log")
 line = fileopen.readline()
 while line:
-    data = json.loads(line)
-    request = data['request']
-    remote_ip = request['remote_ip']
-    timestamp = data['ts']
-    host = request['host']
-    if remote_ip not in connection_analysis:
-        ipdata = json.loads(get_geolocation_info(remote_ip))
-        time.sleep(1)
-        city = ipdata['city']
-        country = ipdata['country']
-        connection = ipdata['connection']
-        isp_name = connection['isp_name']
-        connection_type = connection['connection_type']
-        host_analysis = {host: 1}
-        ipinfo = {'time': time.strftime("%Y-%m-%d %H:%M:%S", (time.localtime(timestamp))),
-                  'host_analysis': host_analysis,
-                  'city': city,
-                  'country': country,
-                  'isp_name': isp_name,
-                  'connection_type': connection_type
-                  }
-        connection_analysis[remote_ip] = ipinfo
-    else:
-        ipinfo = connection_analysis[remote_ip]
-        ipinfo['time'] = time.strftime("%Y-%m-%d %H:%M:%S", (time.localtime(timestamp)))
-        host_analysis = ipinfo['host_analysis']
-        if host not in host_analysis:
-            host_analysis.setdefault(host, 1)
+    try:
+        data = json.loads(line)
+        request = data['request']
+        remote_ip = request['remote_ip']
+        timestamp = data['ts']
+        host = request['host']
+        if remote_ip not in connection_analysis:
+            ipdata = json.loads(get_geolocation_info(remote_ip))
+            time.sleep(1)
+            city = ipdata['city']
+            country = ipdata['country']
+            connection = ipdata['connection']
+            isp_name = connection['isp_name']
+            connection_type = connection['connection_type']
+            host_analysis = {host: 1}
+            ipinfo = {'time': time.strftime("%Y-%m-%d %H:%M:%S", (time.localtime(timestamp))),
+                      'host_analysis': host_analysis,
+                      'city': city,
+                      'country': country,
+                      'isp_name': isp_name,
+                      'connection_type': connection_type
+                      }
+            connection_analysis[remote_ip] = ipinfo
         else:
-            host_analysis[host] += 1
-        ipinfo['host_analysis'] = host_analysis
-        connection_analysis[remote_ip] = ipinfo
-    line = fileopen.readline()
+            ipinfo = connection_analysis[remote_ip]
+            ipinfo['time'] = time.strftime("%Y-%m-%d %H:%M:%S", (time.localtime(timestamp)))
+            host_analysis = ipinfo['host_analysis']
+            if host not in host_analysis:
+                host_analysis.setdefault(host, 1)
+            else:
+                host_analysis[host] += 1
+            ipinfo['host_analysis'] = host_analysis
+            connection_analysis[remote_ip] = ipinfo
+        line = fileopen.readline()
+    except Exception as e:
+        continue
 for ip, analysis in connection_analysis.items():
     host_analysis = analysis['host_analysis']
     plist = sorted(host_analysis.items(), key=lambda x: (x[1], x[0]), reverse=True)
